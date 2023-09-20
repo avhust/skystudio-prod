071187dedf39c91d00e4e9c536e43c76ff5cf287
avhust <victor@avhust.com>
2023-09-20 21:24:00 +0300

    .

--- src/lib/GCalendar.svelte
+++ src/lib/GCalendar.svelte
@@ -1,8 +1,8 @@
 <iframe
-	src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FKiev&mode=MONTH&showTitle=0&showDate=1&showPrint=0&showTabs=1&showCalendars=0&showTz=0&title=SkyStudio%20%D0%91%D1%80%D0%BE%D0%BD%D1%8C&src=c2t5c3R1ZGlvLnV6QGdtYWlsLmNvbQ&color=%234285F4"
+	src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FKiev&mode=WEEK&showTitle=0&showDate=1&showPrint=0&showTabs=1&showCalendars=0&showTz=0&title=SkyStudio%20%D0%91%D1%80%D0%BE%D0%BD%D1%8C&src=c2t5c3R1ZGlvLnV6QGdtYWlsLmNvbQ&color=%234285F4"
 	style="border: 0"
 	width="800"
-	height="600"
+	height="800"
 	frameborder="0"
 	scrolling="no"
 	title="Google Calendar"
@@ -13,7 +13,7 @@
 		margin-bottom: 2rem;
 		width: 100%;
 		height: auto;
-		min-height: 400px;
+		min-height: 600px;
 		max-height: 800px;
 		border-radius: 10px;
 		/* filter: brightness(1.3) invert(0.17) saturate(2.6) sepia(0.25); */
